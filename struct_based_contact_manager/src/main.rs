6bbc5b920f8e3af6d3fc1dbac76dbfe30d10f7e0
ROYMACMINI <roymacmini@ROYMACMINIs-Mac-mini.local>
2026-05-19 08:54:46 +0700

    add contact management struct

--- struct_based_contact_manager/src/main.rs
+++ struct_based_contact_manager/src/main.rs
@@ -11,19 +11,71 @@ struct Contact{
 
 fn main() {
     println!("📒\n Contact manager");
-    printlpn!("1. Add contact");
+    println!("1. Add contact");
     println!("2. View contacts");
     println!("3. Search contacts");
     println!("4. Exit");
 
+    let mut contacts: Vec<Contact> = Vec::new();
+    let mut next_id =1;
+
+    loop {
+        
+    
+
     let choice = input("Enter your choice: ");
-    match coice.trim() {
-        "1" => add_contact(),
-        "2" => view_contacts(),
-        "3" => search_contacts(),
-        "4" => println!("Goodbye!"),
+    match choice.trim() {
+        "1" =>{
+            let name = input("Name :");
+            let phone = input("Phone :");
+            let email = input("Email :");
+            contacts.push(Contact{id:next_id,name,phone,email});
+            println!("Contact added with ID {}",next_id);
+            next_id +=1;
+            
+        },
+        "2" => { 
+            if contacts.is_empty(){
+                println!("No Contacts available ");
+            }else {
+                for c in &contacts{
+                    println!("[{}]|{}|{}|{}",c.id,c.name,c.email,c.phone);
+                }
+            }
+        },
+        "3" => {
+                let query = input("Search by name or email:");
+                let results:Vec<&Contact> = contacts.iter()
+                .filter(|c|c.name.contains(&query)|| c.email.contains(&query)).collect();
+                if results.is_empty(){
+                    println!("No match found");
+                }else {
+                    for c in results{
+                        println!("[{}]|{}|{}|{}",c.id,c.name,c.email,c.phone);
+                    }
+                }
+
+        },
+        "4" => {
+            let id = input("Enter id to delete :")
+            .parse::<usize>().unwrap_or(0);
+            let len_before = contacts.len();
+            contacts.retain(|c|c.id!=id);
+            if contacts.len() < len_before {
+                println!("Contact delete");
+            }else {
+                println!("id not found");
+            }
+        },
+        "5" => {
+            println!("Exiting !");
+            break;
+        },
+        
         _ => println!("Invalid choice, please try again."),
     }
+
+}
 
 }
 
