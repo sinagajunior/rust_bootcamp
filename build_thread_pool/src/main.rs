23a8fa2ab4ef6d0a306ae5004dea9ca9111b014f
ROYMACMINI <roymacmini@ROYMACMINIs-Mac-mini.local>
2026-05-24 21:23:25 +0700

    add buid thread pool

---
+++ build_thread_pool/src/main.rs
@@ -0,0 +1,73 @@
+use std::sync::{Arc, Mutex, mpsc};
+use std::thread;
+
+struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+impl ThreadPool {
+    fn new(size: usize) -> ThreadPool {
+        assert!(size > 0, "pool size must be greate than zero.");
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let workers = (0..size)
+            .map(|id| Worker::new(id, Arc::clone(&receiver)))
+            .collect();
+
+        ThreadPool { workers, sender }
+    }
+
+    fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        self.sender.send(Box::new(f)).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(move || {
+            loop {
+                let message = receiver.lock().unwrap().recv();
+                match message {
+                    Ok(job) => {
+                        println!("Worker {} got a job!", id);
+                        job();
+                    }
+                    Err(_) => {
+                        println!("Worker {} shutting down", id);
+                        break;
+                    }
+                }
+            }
+        });
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
+
+fn main() {
+    println!("Thread pool Demo");
+    let pool = ThreadPool::new(4);
+
+    for i in 1..=8 {
+        pool.execute(move || {
+            println!("Task {} is running ", i);
+            std::thread::sleep(std::time::Duration::from_millis(500));
+            println!("Task {} done", i);
+        });
+    }
+
+    std::thread::sleep(std::time::Duration::from_secs(3));
+}
